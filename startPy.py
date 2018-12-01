4b17f4f5cef674cf33e1e1358987ee43421e5848
Alexander <861289@stud.unive.it>
2018-12-01 19:29:11 +0100

    thread-socket-sender+receiver-to test

--- startPy.py
+++ startPy.py
@@ -4,26 +4,15 @@ import socket
 from _thread import *
 from ev3dev2.socket_manager.deserializer import *
 
-host = ''
-port = 8888
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-try:
-    s.bind((host, port))
-except socket.error as e:
-    print(str(e))
-s.listen(5)
-print('# waiting for an input...\n')
-
 
 def thread_client(sock, conn):
-    print('# server IP:', socket.gethostbyname(socket.gethostname()), '\n')
     conn.send(str.encode('Welcome to the server...\n'))
     while 1:
         data = conn.recv(2048)
         msg = data.decode('utf-8')
         print('->', msg, '<-')
         if not data:
-            waiting()
+            waiting(sock)
         if msg == '#shutdown#':
             print(msg, '-> killing the socket!')
             sock.close()
@@ -39,4 +28,14 @@ def waiting(sock):
         start_new_thread(thread_client, (sock, conn,))
 
 
+host = ''
+port = 8888
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+try:
+    s.bind((host, port))
+except socket.error as e:
+    print(str(e))
+s.listen(10)
+print('# server IP:', socket.gethostbyname(socket.gethostname()), '\n')
+print('# waiting for an input...\n')
 waiting(s)
